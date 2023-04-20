e2c1eefd16aff711a775de034f307fdf5119b1f2
MILKA-WANGA <83470011+MILKA-WANGA@users.noreply.github.com>
2023-04-20 04:58:24 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+//Import http 
+//const http =require('http');
+const express=require('express');
+const app=express();
+app.use('/addProduct',(req,res,next)=>
+{
+console.log('In another middleware');
+res.send('<h1>The Add Product</h1>');
+});
+
+app.use('/',(req,res,next)=>
+{
+console.log('In another middleware');
+res.send('<h1>Hello from express</h1>');
+});
+
+
+//const server1= http.createServer(app); 
+//set the port the server to listen at
+app.listen(2000);
+
+
